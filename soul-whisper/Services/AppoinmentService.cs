fa8000ab9fe24e0dbda9529bfcc58d26d5f6bf7e
BaoNguyen <toibaonguyen@gmail.com>
2023-12-19 11:06:07 +0700

    hehehehehe fix đc gòi

--- soul-whisper/Services/AppoinmentService.cs
+++ soul-whisper/Services/AppoinmentService.cs
@@ -3,11 +3,11 @@ using soul_whisper.Models.Public;
 namespace soul_whisper.Service;
 
 public class AppoinmentService{
-    public async Task<AppoinmentDTO> GetAppointments(string userId)
+    public async Task<AppointmentDTO> GetAppointments(string userId)
     {
 
     }
-    public async Task<AppoinmentDTO> GetAppointment(string id)
+    public async Task<AppointmentDTO> GetAppointment(string id)
     {
 
     }
